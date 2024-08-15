2031e960227d85cdb1dc35148eaa94aff4b5a02d
youcef <gagiboss60@gmail.com>
2024-08-15 18:34:47 +0100

    first commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://stirring-meerkat-39.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default authConfig;
---
+++ convex/openai.ts
@@ -0,0 +1,49 @@
+"use server"
+import { action } from "./_generated/server";
+import { v } from "convex/values";
+
+import OpenAI from "openai";
+// @ts-ignore
+import { SpeechCreateParams } from "openai/resources/audio/speech.mjs";
+
+const openai = new OpenAI({
+  apiKey: "sk-hKwN1eZh0hVM_2BLQB7eQdgjbqEJ5r0-gpqhXaNUMBT3BlbkFJIf8DNaNnCMv528Dmi2jGMmECpKvb0whpbIvtdHnw4A",
+})
+
+export const generateAudioAction = action({
+  args: { input: v.string(), voice: v.string() },
+  handler: async (_, { voice, input }) => {
+    const mp3 = await openai.audio.speech.create({
+      model: "tts-1",
+      voice: voice as SpeechCreateParams['voice'],
+      input,
+    });
+
+    const buffer = await mp3.arrayBuffer();
+    
+    return buffer;
+  },
+});
+
+export const generateThumbnailAction = action({
+  args: { prompt: v.string() },
+  handler: async (_, { prompt }) => {
+    const response = await openai.images.generate({
+      model: 'dall-e-3',
+      prompt,
+      size: '1024x1024',
+      quality: 'standard',
+      n: 1,
+    })
+
+    const url = response.data[0].url;
+
+    if(!url) {
+      throw new Error('Error generating thumbnail');
+    }
+
+    const imageResponse = await fetch(url);
+    const buffer = await imageResponse.arrayBuffer();
+    return buffer;
+  }
+})
